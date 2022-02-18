75855afdaabb732e0b8ee1c731527601dc04e6c9
Sergei Sorokin <sench32@gmail.com>
2022-02-18 18:33:56 +0200

    Layout according to the task (43. Flutter Layouts Challenge) reproduced

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,42 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(width: 100, color: Colors.red,),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                ),
+              ],
+            ),
+            Container(width: 100, color: Colors.blue,),
+          ],
+        ),
+      ),
     );
   }
 }
