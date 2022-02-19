e82b69b6fa1e1b1f732840779e077d40cc1b9fad
Sergei Sorokin <sench32@gmail.com>
2022-02-19 14:27:02 +0200

    Created user card with ListTile widget

--- lib/main.dart
+++ lib/main.dart
@@ -27,33 +27,76 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.teal,
       body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:  <Widget>[
-              const CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('images/100_years.jpg'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:  <Widget>[
+            const CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('images/100_years.jpg'),
+            ),
+            const Text(
+              'User Name',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                  fontSize: 24,
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                  fontSize: 16,
+                  color: Colors.teal.shade100,
+                letterSpacing: 2.5,
+                  ),
+            ),
+            SizedBox(
+              width: 250,
+              height: 16,
+              child: Divider(color: Colors.teal.shade100,),
+            ),
+            Card(
+              color: Colors.white,
+              margin: const EdgeInsets.symmetric(
+                vertical: 8,
+                horizontal: 16,
               ),
-              const Text(
-                'Name LastName',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                    fontSize: 24,
-                    color: Colors.white,
-                    fontWeight: FontWeight.normal),
+              child: ListTile(
+                leading: const Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '+380 68 123 4567',
+                  style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 16.0),
+                ),
               ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                  fontFamily: 'Source Sans Pro',
-                    fontSize: 16,
-                    color: Colors.teal.shade100,
-                  letterSpacing: 2.5,
-                    ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: const EdgeInsets.symmetric(
+                vertical: 8,
+                horizontal: 16,
               ),
-            ],
-          ),
+              child: ListTile(
+                leading: const Icon(
+                  Icons.mail,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'userMail@email.com',
+                  style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 16.0),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
