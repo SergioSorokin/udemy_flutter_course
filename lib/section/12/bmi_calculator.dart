324133fddc980abb8ad94744c37915b9ae73e276
Sergei Sorokin <sench32@gmail.com>
2022-05-20 14:40:55 +0300

    created class InputPage

--- lib/section/12/bmi_calculator.dart
+++ lib/section/12/bmi_calculator.dart
@@ -6,6 +6,35 @@ class BMICalculator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const InputPage();
+  }
+}
+
+class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'BMI Calculator',
+          style: TextStyle(color: Colors.pink, fontSize: 32),
+        ),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
   }
 }
