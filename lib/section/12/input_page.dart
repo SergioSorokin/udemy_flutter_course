ae20a76af735797195a6fcf99d78d526150f1578
Sergei Sorokin <sench32@gmail.com>
2022-05-31 22:40:18 +0300

    created IconContent class

--- lib/section/12/input_page.dart
+++ lib/section/12/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColour = Color(0xFF1D1E33);
@@ -30,11 +31,19 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     colour: activeCardColour,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      title: 'MALE',
+                    ),
                   ),
                 ),
-                Expanded(
+                 Expanded(
                   child: ReusableCard(
                     colour: activeCardColour,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      title: 'FEMALE',
+                    ),
                   ),
                 ),
               ],
@@ -73,6 +82,41 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class IconContent extends StatelessWidget {
+  const IconContent({
+     required this.icon,
+     required this.title,
+    Key? key,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children:  <Widget>[
+        Icon(
+          icon,
+          color: Colors.white,
+          size: 80,
+        ),
+       const SizedBox(
+          height: 15,
+        ),
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 18,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
   const ReusableCard({
     Key? key,
@@ -87,6 +131,7 @@ class ReusableCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(15.0),
+      child: cardChild,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),
         color: colour,
