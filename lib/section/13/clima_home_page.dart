88216d75e29709a638fa546022069aa90669db68
Sergei Sorokin <sench32@gmail.com>
2022-06-16 01:28:15 +0300

    started section 13 "Clima"

---
+++ lib/section/13/clima_home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ClimaHomePage extends StatelessWidget {
+  static const String routName = '/Clima';
+  const ClimaHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
