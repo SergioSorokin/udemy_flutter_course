2d1e1fb1525a582af817101efb50a8e057d6057c
Sergei Sorokin <sench32@gmail.com>
2022-06-30 19:50:42 +0300

    created CityScreen class with background image and two buttons

---
+++ lib/section/13/screens/city_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:udemy_flutter_course/section/13/utilities/constans.dart';
+
+class CityScreen extends StatefulWidget {
+  const CityScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CityScreen> createState() => _CityScreenState();
+}
+
+class _CityScreenState extends State<CityScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/city_background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        constraints: const BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: ElevatedButton(
+                  onPressed: () {
+                  },
+                  child: const Icon(
+                    Icons.arrow_back_ios,
+                    size: 50.0,
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      Colors.transparent,
+                    ),
+                    shadowColor: MaterialStateProperty.all<Color>(
+                      Colors.transparent,
+                    ),
+                    overlayColor: MaterialStateProperty.all<Color>(
+                      Colors.transparent,
+                    ),
+                    splashFactory: NoSplash.splashFactory,
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                },
+                child: const Text(
+                  'Get Weather',
+                  style: kButtonTextStyle,
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    Colors.transparent,
+                  ),
+                  shadowColor: MaterialStateProperty.all<Color>(
+                    Colors.transparent,
+                  ),
+                  overlayColor: MaterialStateProperty.all<Color>(
+                    Colors.transparent,
+                  ),
+                  splashFactory: NoSplash.splashFactory,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
