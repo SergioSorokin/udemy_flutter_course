5e4fbd40865221c4eb9be0e1fc1ea7a273436377
Sergei Sorokin <sench32@gmail.com>
2022-07-13 15:08:45 +0300

    implemented modal progress hud in the registration screen

--- lib/section/15/screens/flash_chat_registration_screen.dart
+++ lib/section/15/screens/flash_chat_registration_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_alt/modal_progress_hud_alt.dart';
 import 'package:udemy_flutter_course/section/15/components/rounded_button.dart';
 import 'package:udemy_flutter_course/section/15/flash_chat_constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -15,6 +16,7 @@ class FlashChatRegistrationScreen extends StatefulWidget {
 
 class _FlashChatRegistrationScreenState
     extends State<FlashChatRegistrationScreen> {
+  bool showSpinner = false;
   final _auth = FirebaseAuth.instance;
   late String email;
   late String password;
@@ -23,62 +25,71 @@ class _FlashChatRegistrationScreenState
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'flash-logo',
-              child: SizedBox(
-                height: 200.0,
-                child: Image.asset('images/flash-logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'flash-logo',
+                child: SizedBox(
+                  height: 200.0,
+                  child: Image.asset('images/flash-logo.png'),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                email = value;
-              },
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.emailAddress,
-              style: const TextStyle(color: Colors.black),
-              decoration:
-                  kTextFieldDecoration.copyWith(hintText: 'Enter your email'),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                password = value;
-              },
-              textAlign: TextAlign.center,
-              obscureText: true,
-              style: const TextStyle(color: Colors.black),
-              decoration: kTextFieldDecoration.copyWith(
-                  hintText: 'Enter your password.'),
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-              title: 'Register',
-              color: Colors.blueAccent,
-              onPressed: () async {
-                try {
-                   await _auth.createUserWithEmailAndPassword(
-                      email: email, password: password);
-                  Navigator.pushNamed(context, FlashChatScreen.routName);
-                } catch (e) {
-                  print(e);
-                }
-              },
-            ),
-          ],
+              const SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  email = value;
+                },
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+                style: const TextStyle(color: Colors.black),
+                decoration:
+                    kTextFieldDecoration.copyWith(hintText: 'Enter your email'),
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  password = value;
+                },
+                textAlign: TextAlign.center,
+                obscureText: true,
+                style: const TextStyle(color: Colors.black),
+                decoration: kTextFieldDecoration.copyWith(
+                    hintText: 'Enter your password.'),
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                title: 'Register',
+                color: Colors.blueAccent,
+                onPressed: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  try {
+                    await _auth.createUserWithEmailAndPassword(
+                        email: email, password: password);
+                    Navigator.pushNamed(context, FlashChatScreen.routName);
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  } catch (e) {
+                    print(e);
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
