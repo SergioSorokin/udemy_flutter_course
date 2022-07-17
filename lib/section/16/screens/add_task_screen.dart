b650d519d1c5b3bd07709e0c04e132bab2e15bf6
Sergei Sorokin <sench32@gmail.com>
2022-07-17 20:21:54 +0300

    created bottom sheet design for adding tasks

---
+++ lib/section/16/screens/add_task_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  const AddTaskScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xff757575),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.lightBlueAccent, fontSize: 30),
+            ),
+            const TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.black),
+              cursorColor: Colors.lightBlueAccent,
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.lightBlueAccent),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.lightBlueAccent),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Add'),
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(Colors.lightBlueAccent),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
